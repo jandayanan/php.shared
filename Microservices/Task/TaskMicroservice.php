2bc74de69826ec4ad90cc9d5216f9ba2dbfd8a08
Glyde <e20v.glyde@gmail.com>
2021-08-25 10:42:15 +0800

    Glyde > added Task Service

---
+++ Microservices/Task/TaskMicroservice.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Shared\Microservices\Task;
+
+use Shared\BaseClasses\Microservice;
+
+/**
+ * Class AccountsHost
+ * @package App\Data\Providers\Microservice
+ *
+ *
+ */
+class TaskMicroservice extends Microservice
+{
+    public function __construct()
+    {
+        $this->app_slug = "services.task.app.code";
+        $this->base_url = env("SCHEDULE_API_ENDPOINT", 'http://e20v.svc.task');
+        $this->options = [];
+        $this->auth_flag = true;
+
+        parent::__construct();
+    }
+}
