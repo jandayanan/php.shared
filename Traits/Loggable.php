41b1987089b6ded251fd7db1e65a2dabfed94477
Jan Koichi Dayanan <jandayanan@gmail.com>
2022-09-13 23:59:05 +0800

    Jan > SHared > Added loggable trait

---
+++ Traits/Loggable.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jan
+ * Date: 9/13/22
+ * Time: 11:57 PM
+ */
+
+namespace Shared\Traits;
+
+
+use Illuminate\Support\Facades\Log;
+
+trait Loggable
+{
+    protected function log( $message, $data, $channel='single', $type = 'info' ){
+        $message = wrap_with ( self::class ) . " " . $message;
+        Log::channel( $channel )
+            ->$type( $message, $data );
+    }
+}
