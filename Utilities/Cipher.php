f5521de8b63c3b4260edaceb099ea553b7163be6
Jan Koichi Dayanan <jandayanan@gmail.com>
2022-09-29 01:45:22 +0800

    Jan > Added log on cipher

--- Utilities/Cipher.php
+++ Utilities/Cipher.php
@@ -4,11 +4,17 @@
 namespace Shared\Utilities;
 
 
+use Illuminate\Support\Facades\Log;
+
 class Cipher
 {
-    protected static $salt = "viewqwest.com";
+    protected static $salt = "test-app";
 
     public static function hash( $value ){
+        Log::info( "CIPHER LOG", [
+            "salt" => self::$salt,
+            "value" => $value,
+        ]);
         return app_hash( self::$salt . $value );
     }
 }
